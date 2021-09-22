6ec544cc8a59928af1a2eb9df0f888d5766bf458
Argusmocny <k.kaminski@cent.uw.edu.pl>
2021-09-22 17:51:50 +0200

    converted mulithead operations into tensors and added new description

--- egat/edge_gat_learnable.py
+++ egat/edge_gat_learnable.py
@@ -3,19 +3,38 @@ import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 
+class MultiHeadEGATLayer(nn.Module):
+    """
+    Parameters
+    ----------
+    in_node_feats : int, or pair of ints
+        Input node feature size.
+    in_edge_feats : int, or pair of ints
+        Input edge feature size.
+    out_node_feats : int
+        Output nodes feature size.
+    out_edge_feats : int
+        Output edge feature size.
+    num_heads : int
+        Number of attention heads.
 
-
-
-class GATLayer(nn.Module):
-    '''
-    single head variation of edge-gat
-    '''
-    def __init__(self, in_dim_n, in_dim_e, out_dim_n, out_dim_e, attention_scaler = 'softmax', **kw_args):
+    """
+    def __init__(self,
+                 in_node_feats,
+                 in_edge_feats,
+                 out_node_feats,
+                 out_edge_feats,
+                 num_heads,
+                 attention_scaler = 'softmax',
+                 **kw_args):
         
         super().__init__()
-        self.fc = nn.Linear(in_dim_n, out_dim_n, bias=False)
-        self.attn_fc_edge = nn.Linear(in_dim_e + 2 * out_dim_n, out_dim_e, bias=True)
-        self.attn_fc_coef = nn.Linear(out_dim_e, 1, bias=False)
+        self._num_heads = num_heads
+        self._out_node_feats = out_node_feats
+        self._out_edge_feats = out_edge_feats
+        self.fc_nodes = nn.Linear(in_node_feats, out_node_feats*num_heads, bias=True)
+        self.fc_edges = nn.Linear(in_edge_feats + 2*in_node_feats, out_edge_feats*num_heads, bias=True)
+        self.fc_attn = nn.Linear(out_edge_feats, num_heads, bias=False)
         self.reset_parameters()
         self.attention_scaler = attention_scaler
         if self.attention_scaler == 'sigmoid':
@@ -26,87 +45,76 @@ class GATLayer(nn.Module):
         Reinitialize learnable parameters.
         """
         gain = nn.init.calculate_gain('relu')
-        nn.init.xavier_normal_(self.attn_fc_edge.weight, gain=gain)
-        nn.init.xavier_normal_(self.attn_fc_coef.weight, gain=gain)
+        nn.init.xavier_normal_(self.fc_nodes.weight, gain=gain)
+        nn.init.xavier_normal_(self.fc_edges.weight, gain=gain)
+        nn.init.xavier_normal_(self.fc_attn.weight, gain=gain)
 
     def edge_attention(self, edges):
         #extract features
-        src_data = edges.src['z']
-        dst_data = edges.dst['z']
-        feat_data = edges.data['feats']
-        #merge node_i - edge_ij - node_j features
-        stacked = torch.cat([src_data, feat_data, dst_data], dim=1)
+        h_src = edges.src['h']
+        h_dst = edges.dst['h']
+        f = edges.data['f']
+        stack = torch.cat([h_src, f, h_dst], dim=-1)
         # apply FC and activation
-        feat_data = self.attn_fc_edge(stacked)
-        feat_data = F.leaky_relu(feat_data)
+        f_out = self.fc_edges(stack)
+        f_out = F.leaky_relu(f_out)
+        f_out = f_out.view(-1, self._num_heads, self._out_edge_feats)
         # FC to reduce edge_feats to scalar
-        a = self.attn_fc_coef(feat_data)
-        return {'attn': a, 'feats' : feat_data}
+        a = self.fc_attn(f_out).sum(-1).unsqueeze(-1)
+        #print(a.shape)
+        return {'a': a, 'f' : f_out}
 
     def message_func(self, edges):
-        return {'z': edges.src['z'], 'attn': edges.data['attn']}
+        return {'h': edges.src['h'], 'a': edges.data['a']}
 
     def reduce_func(self, nodes):
         if self.attention_scaler == 'sigmoid':
-            alpha = self.scaler(nodes.mailbox['attn'])
+            alpha = self.scaler(nodes.mailbox['a'])
         else:
-            alpha = F.softmax(nodes.mailbox['attn'], dim=1)
-        h = torch.sum(alpha * nodes.mailbox['z'], dim=1)
+            alpha = F.softmax(nodes.mailbox['a'], dim=1)
+        h = torch.sum(alpha * nodes.mailbox['h'], dim=1)
         return {'h': h}
 
     def forward(self, g, nfeats, efeats):
-        z = self.fc(nfeats)
-        g.edata['feats'] = efeats
-        g.ndata['z'] = z
+        r"""
+        Compute new node and edge features.
+
+        Parameters
+        ----------
+        graph : DGLGraph
+            The graph.
+        nfeats : torch.Tensor or pair of torch.Tensor
+            If a torch.Tensor is given, the input node feature of shape :math:`(N, D_{in})`
+            where:
+                :math:`D_{in}` is size of input node feature,
+                :math:`N` is the number of nodes.
+            If a pair of torch.Tensor is given, the pair must contain two tensors of shape
+            :math:`(N_{in}, D_{in_{src}})` and :math:`(N_{out}, D_{in_{dst}})`.
+        efeats : torch.Tensor
+            
+        Returns
+        -------
+        pair of torch.Tensor
+            node output features followed by edge output features
+            The node output feature of shape :math:`(N, H, D_{out})` 
+            The edge output feature of shape :math:`(S, H, F_{out})`
+            where:
+                :math:`H` is the number of heads,
+                :math:`D_{out}` is size of output node feature,
+                :math:`F_{out}` is size of output edge feature.
+        """
+        ##TODO allow node src and dst feats
+        g.edata['f'] = efeats
+        g.ndata['h'] = nfeats
+        
         g.apply_edges(self.edge_attention)
+        
+        nfeats_ = self.fc_nodes(nfeats)
+        nfeats_ = nfeats_.view(-1, self._num_heads, self._out_node_feats)
+        
+        g.ndata['h'] = nfeats_
         g.update_all(message_func = self.message_func,
                      reduce_func = self.reduce_func)
-        return g.ndata.pop('h'), g.edata.pop('feats')
-    
-    
-class MultiHeadEGATLayer(nn.Module):
-    '''
-    Multihead version of Edge-GAT layer. Variation over Deep Graph Library GAT tutorial:
-    https://docs.dgl.ai/en/0.4.x/tutorials/models/1_gnn/9_gat.html
-    Tips: 
-        * avoid high dimensionality of `out_dim_e` - increases computations time
-        * edge features are returned in same form as nodes that is (Batch, num_heads, out_dim_e)
-    params:
-        most of them are same as in regular dgl.nn.pytorch.GATConv
-            in_dim_e (int) number of input edge features
-            out_dim_e (int) number of output edge features
-            in_dim_n (int) number of input node features
-            out_dim_n (int) number of output node features
-            num_heads (int) number of attention heads 
-            activation (None, or torch activation eg: F.relu) default None
-                activation after concatenation
-            attention_scaler (str) `sigmoid` or `softmax` - tells how to scale attention
-                coefficients
-            
-    returns:
-        (node_features, edge_features) (tuple of torch.(cuda)FloatTensor's)
-    '''
-    def __init__(self,  in_dim_n, in_dim_e, out_dim_n, out_dim_e, num_heads, \
-                 activation=None,attention_scaler='softmax', **kw_args):
-        super().__init__()
-        self.heads = nn.ModuleList()
-        self.activation = activation
-        for i in range(num_heads):
-            self.heads.append(GATLayer(in_dim_n, in_dim_e, out_dim_n, out_dim_e, attention_scaler))
         
-    def forward(self, g, nfeats, efeats):
-        nodes_stack, edges_stack = [], []
-        for attn_head in self.heads:
-            nodes, edges = attn_head(g, nfeats, efeats)
-            nodes_stack.append(nodes.unsqueeze(1))
-            edges_stack.append(edges.unsqueeze(1))
-            
-        nodes_stack = torch.cat(nodes_stack, dim=1)
-        edges_stack = torch.cat(edges_stack, dim=1)
-        if self.activation is not None:
-            nodes_stack =  self.activation(nodes_stack)
-            edges_stack = self.activation(edges_stack)
-        return nodes_stack, edges_stack
+        return g.ndata.pop('h'), g.edata.pop('f')
     
-            
-        
